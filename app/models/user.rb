e0ad206aac944f18eb616a7baa2a55df5731e419
Mary Huang <rhymeandreason1.0@gmail.com>
2012-07-04 13:46:17 -0400

    took out password confirmation on sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,13 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :username, :email, :avatar, :avatar_file_name, :password, :password_confirmation, :remember_me, :invite_code
+  attr_accessible :username, :email, :avatar, :avatar_file_name, :password, :remember_me, :invite_code
   attr_accessor :invite_code
   # attr_accessible :title, :body
   
   validates_each :invite_code, :on => :create do |record, attr, value|
       record.errors.add attr, "Please enter correct invite code" unless
-        value && value == "MASHABLE34" || value == "KICKSTARTER56"
+        value && value == "MASHABLE34" || value == "KICKSTARTER56" || value == "CONSTRVCTIT"
   end
   
   mount_uploader :avatar, AvatarUploader
