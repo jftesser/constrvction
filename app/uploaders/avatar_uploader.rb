c7918c2f3d34198bdbe66f98449277f8f4313e57
Mary Huang <rhymeandreason1.0@gmail.com>
2012-07-04 22:48:01 -0400

    default profile image

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -21,10 +21,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
    def default_url
-     # For Rails 3.1+ asset pipeline compatibility:
+    #For Rails 3.1+ asset pipeline compatibility:
     #asset_path("fallback/" + [version_name, "girl_profile.jpg"].compact.join('_'))
   #
-     "images/fallback/" + [version_name, "girl_profile.jpg"].compact.join('_')
+    "fallback/" + [version_name, "girl_profile.jpg"].compact.join('_')
    end
 
   # Process files as they are uploaded:
